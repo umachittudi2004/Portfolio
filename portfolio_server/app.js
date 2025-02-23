f8ccf763906ca8ca5cb8612a6356fa610ebdc661
umachittudi2004 <umachittudi2004@gmail.com>
2025-02-23 11:52:33 +0530

    new one-2

--- portfolio_server/app.js
+++ portfolio_server/app.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({origin:"*"}));
 app.use(express.json());
 
 // Nodemailer transporter setup
